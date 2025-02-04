bfd762dee43b914ebaa666cf9faf54840fe19da6
Dylan Bartell <barteldf@sunypoly.edu>
2025-02-04 12:09:00 -0500

    Initial commit with bottom-banner 3 page navigation

---
+++ app/src/main/java/com/example/budgetapp2/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.example.budgetapp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.budgetapp2.ui.theme.BudgetApp2Theme
+import androidx.navigation.NavGraph.Companion.findStartDestination
+
+
+
+
+sealed class Screen(val route: String, val title: String) {
+    object Home: Screen("home", "Home")
+    object BudgetList: Screen("budgetList", "Budget List")
+    object Settings: Screen("settings", "Settings")
+}
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BudgetApp2Theme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+        innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.BudgetList.route) { BudgetListScreen() }
+            composable(Screen.Settings.route) { SettingsScreen() }
+
+        }
+
+    }
+
+}
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home")
+}
+
+@Composable
+fun BudgetListScreen() {
+    Text(text = "Budget List")
+}
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "Settings")
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val route: String
+)
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            icon = Icons.Default.Home,
+            route = Screen.Home.route
+        ),
+        BottomNavigationItem(
+            title = "Budget List",
+            icon = Icons.Default.List,
+            route = Screen.BudgetList.route
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            icon = Icons.Default.Settings,
+            route = Screen.Settings.route
+        )
+    )
+
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BudgetApp2Theme {
+        Greeting("Android")
+    }
+}
+
+
