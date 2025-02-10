856b4408fe842ff7cec2d0490798ca97b3ff9fe7
Dylan Bartell <barteldf@sunypoly.edu>
2025-02-10 16:26:26 -0500

    Reformatting of system hierarchy

---
+++ app/src/main/java/com/example/budgetapp2/ui/MainScreen.kt
@@ -0,0 +1,115 @@
+package com.example.budgetapp2.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.budgetapp2.Screen
+import com.example.budgetapp2.ui.theme.BudgetApp2Theme
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+            innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.BudgetList.route) { BudgetListScreen() }
+            composable(Screen.Settings.route) { SettingsScreen() }
+
+        }
+
+    }
+
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val icon: ImageVector,
+    val route: String
+)
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home")
+}
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "Settings")
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            icon = Icons.Default.Home,
+            route = Screen.Home.route
+        ),
+        BottomNavigationItem(
+            title = "Budget List",
+            icon = Icons.Default.Menu,
+            route = Screen.BudgetList.route
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            icon = Icons.Default.Settings,
+            route = Screen.Settings.route
+        )
+    )
+
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BudgetApp2Theme {
+        MainScreen()
+    }
+}
